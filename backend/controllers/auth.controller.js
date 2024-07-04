0835d934d52d49bdc0e0a30a1ec05a0cde6536a5
Roi Sasson <roisa@mta.ac.il>
2024-07-05 00:28:36 +0300

    post method success

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,11 +2,52 @@
 // inside the consts we send requests and their body
 // for example, in sign-up we will have the JSON format with all the details of sign up
 
+import User from '../models/user.model.js';
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
     try {
-        const {fullName, userName, password, confirmPassword, gender} = req.body;
-    } catch {
+        const {fullName, username, password, confirmPassword, gender} = req.body;
 
+        if (password !== confirmPassword)
+        {
+            return res.status(400).json({error: "Password don't match"})
+        }
+
+        const user = await User.findOne({username}); // check if exists in db
+        if (user)
+        {
+            return res.status(400).json({error: "Username already exists"})
+        }
+
+        // HASH PASSWORD HERE
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // profile picture
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = await User({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic:girlProfilePic
+        })
+
+        await newUser.save(); // save user to db
+        res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            }
+        )
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({error: "Internal Server Error"});
     }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => { 
-    // root route http://localhost:5000/
-    res.send("Hello world!!")
-})
+dotenv.config();
+
+// this will allow us to parse the incoming requests with JSON payloads (from req.body)
+app.use(express.json());
 
 // if we visit /api/auth/login 
 // then the function in auth.routes.js of router.get("/login") will run
 app.use("/api/auth", authRoutes)
 
+//app.get("/", (req, res) => { 
+//    // root route http://localhost:5000/
+//    res.send("Hello world!!")
+//})
 
 app.listen(PORT, () => {
     connectToMongoDB();

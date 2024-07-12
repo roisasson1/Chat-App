82302cf035ff1c92928eed7793803b544072b125
Roi Sasson <roisa@mta.ac.il>
2024-07-13 01:16:20 +0300

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
@@ -10,6 +11,8 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -23,10 +26,17 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-//app.get("/", (req, res) => { 
-//    // root route http://localhost:5000/
-//    res.send("Hello world!!")
-//})
+// gives us the absolute path to the root folder (=dirname)
+// access to dist folder = will be exist after we will build our app
+// will be done in the server when we deploy our app
+// use this express static middleware to serve static files
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+// send the html file that we have in the frontend
+// with this we can run our frontend from our server as well
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
